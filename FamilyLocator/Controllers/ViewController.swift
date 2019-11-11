43bf21fdb1a325195a10e1a75c7cf3c30767fb95
Action Trainee <actiontrainee@devg-cpu-0916s-macbook-pro.local>
2019-11-11 17:35:51 +0800

    wew

--- FamilyLocator/Controllers/ViewController.swift
+++ FamilyLocator/Controllers/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        UILabel.appearance().font = UIFont(name: "Marker Felt Wide", size: 16)
+        
     }
 
 
